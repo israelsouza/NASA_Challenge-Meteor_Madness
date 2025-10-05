dcc1a209f0e150e3ed4abd5eb682471d765bd2db
Israel de Souza <israellimas@hotmail.com>
2025-10-05 00:47:29 -0300

    adiciona menu lateral e captura de coordenadas, e tipos de asteroide

--- web/src/components/Mapa.jsx
+++ web/src/components/Mapa.jsx
@@ -9,6 +9,10 @@ const Mapa = () => {
   const map = useRef(null);
 
   const [mostrarMapa, setMostrarMapa] = useState(true);
+  const [impactLocation, setImpactLocation] = useState(null);
+  
+  const [asteroid, setAsteroid] = useState(null);
+
 
   useEffect(() => {
     if (!mostrarMapa || map.current) return;
@@ -21,6 +25,8 @@ const Mapa = () => {
       projection: "globe",
     });
 
+    map.current.addControl(new mapboxgl.NavigationControl());
+
     map.current.on("style.load", () => {
       map.current.setFog({
         color: "rgba(186, 210, 235, 0.6)",
@@ -28,6 +34,50 @@ const Mapa = () => {
         "space-color": "#000000",
         "horizon-blend": 0.2,
         "star-intensity": 0.2,
+      });
+    });
+
+    map.current.on("click", (e) => {
+      const { lng, lat } = e.lngLat;
+      if (lat < -85 || lat > 85) return;
+
+      const sourceId = "ping-source";
+
+      if (map.current.getLayer("ping-layer")) {
+        map.current.removeLayer("ping-layer");
+        map.current.removeSource(sourceId);
+      }
+
+      map.current.addSource(sourceId, {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              geometry: { type: "Point", coordinates: [lng, lat] },
+            },
+          ],
+        },
+      });
+
+      map.current.addLayer({
+        id: "ping-layer",
+        type: "circle",
+        source: sourceId,
+        paint: {
+          "circle-radius": 10,
+          "circle-color": "red",
+          "circle-opacity": 0.8,
+          "circle-stroke-color": "white",
+          "circle-stroke-width": 2,
+        },
+      });
+
+      setImpactLocation({
+        lng: lng.toFixed(2),
+        lat: lat.toFixed(2),
+        name: `Lng: ${lng.toFixed(2)}, Lat: ${lat.toFixed(2)}`,
       });
     });
 
@@ -48,6 +98,47 @@ const Mapa = () => {
   return (
     <div className={styles.container}>
       <div ref={mapContainer} className={styles.mapa}></div>
+      <div className={styles.menuLateral}>
+        <h2 className={styles.tituloMenu}>Veja o impacto do asteroide</h2>
+ 
+        <div className={styles.informacoes}>
+          <div>
+            <span className={styles.negrito}>Configurações do asteroide</span>
+          </div>
+          <div className={styles.linhaInfo}>
+            <span>Asteroide:</span>
+            <select
+              value={asteroid}
+              onChange={(e) => setAsteroid(e.target.value)}
+            >
+              <option value="Apophis">Apophis</option>
+              <option value="Bennu">Bennu</option>
+              <option value="Ryugu">Ryugu</option>
+            </select>
+          </div>
+
+          <br />
+          <div className={styles.localizacao}>
+            <span>Local do Impacto:</span> <br />
+            <span>
+              {impactLocation ? impactLocation.name : "Selecione no mapa"}
+            </span>
+          </div>
+        </div>
+        <button
+          className={styles.botao}
+          onClick={() => {
+            if (!impactLocation) {
+              alert("Insira um local primeiro para proseguir");
+            } else {
+              alert(`Asteroide lançado em ${impactLocation.name}!`);
+            }
+          }}
+        >
+          Jogar asteroide
+        </button>
+        <div className={styles.instrucao}>selecione local de impacto</div>
+      </div>
     </div>
   );
 };
